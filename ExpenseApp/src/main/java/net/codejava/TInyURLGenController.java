adfc52546a344866136748032caf8ec00f0826db
sammetas <raamaalayam@gmail.com>
2021-05-08 14:10:09 +0530

    Tinyurl use case and sample crud

--- ExpenseApp/src/main/java/net/codejava/TInyURLGenController.java
+++ ExpenseApp/src/main/java/net/codejava/TInyURLGenController.java
@@ -2,11 +2,15 @@ package net.codejava;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,7 +39,8 @@ public class TInyURLGenController {
            System.out.println("Saved sucessfully");
        }
       }else{
-           Optional<TinyUrl> existing=isExists.stream().filter(t->mainUrl.getMainUrl().equals(t.getMainUrl())).findFirst();
+           Optional<TinyUrl> existing=isExists.stream()
+                   .filter(t->mainUrl.getMainUrl().equals(t.getMainUrl())).findFirst();
 
           return new ResponseEntity<TinyUrl>(existing.get(),HttpStatus.OK);
       }
@@ -44,6 +49,82 @@ public class TInyURLGenController {
         return new ResponseEntity<TinyUrl>(mainUrl, HttpStatus.OK);
     }
 
+
+
+    @RequestMapping(path="/redirect/{tinyurl1}/{tinyurl2}")
+    @ResponseBody
+    public ResponseEntity<Object> getRedirect(@PathVariable("tinyurl1") String tinyurl1 ,
+                                           @PathVariable("tinyurl2") String tinyurl2) throws MalformedURLException, URISyntaxException {
+
+        String sMainUrl=  "www.nolinkfound.com";
+String tinyurl=tinyurl1+"/"+tinyurl2;
+        System.out.println("redirect meth"+tinyurl);
+        if(null!= tinyurl &&  !"".equals(tinyurl)){
+
+            List<TinyUrl> list= tInyUrlRepository.findMainUrlByTinyUrl(tinyurl);
+            if(list.size()>0){
+                 Optional<TinyUrl> tinyUrld=list.stream().findFirst();
+                sMainUrl=tinyUrld.get().getMainUrl();
+                System.out.println("Main url::"+sMainUrl);
+            }
+
+            try{
+                URL url = new URL("https://"+sMainUrl);
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setLocation(url.toURI());
+                return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+              //  return new ResponseEntity<URL>(url,HttpStatus.OK);}catch (MalformedURLException e){
+                //e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        URL url = new URL("https://"+sMainUrl);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(url.toURI());
+        return new ResponseEntity<>(httpHeaders,HttpStatus.OK);
+    }
+
+
+    //version 2
+    @RequestMapping(path="/redirect/v1/{tinyurl1}/{tinyurl2}")
+    @ResponseBody
+    public ResponseEntity<Object> getRedirectV1(@PathVariable("tinyurl1") String tinyurl1 ,
+                                              @PathVariable("tinyurl2") String tinyurl2) throws MalformedURLException, URISyntaxException {
+
+        String sMainUrl=  "www.nolinkfound.com";
+        String tinyurl=tinyurl1+"/"+tinyurl2;
+        System.out.println("redirect meth"+tinyurl);
+        if(null!= tinyurl &&  !"".equals(tinyurl)){
+
+      //     TinyUrl tinyRep= tInyUrlRepository.getByTinyUrl(tinyurl);
+            List<TinyUrl> tinyRep= tInyUrlRepository.findByTinyUrl(tinyurl);
+
+
+                sMainUrl=tinyRep.get(0).getMainUrl();
+                System.out.println("Main url::"+sMainUrl);
+
+
+            try{
+                URL url = new URL("https://"+sMainUrl);
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setLocation(url.toURI());
+                return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+                //  return new ResponseEntity<URL>(url,HttpStatus.OK);}catch (MalformedURLException e){
+                //e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        URL url = new URL("https://"+sMainUrl);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(url.toURI());
+        return new ResponseEntity<>(httpHeaders,HttpStatus.OK);
+    }
     private String getFullTiny(String tiny) {
         return "www.by.com/"+tiny;
     }
--- ExpenseApp/src/main/java/net/codejava/TInyUrlRepository.java
+++ ExpenseApp/src/main/java/net/codejava/TInyUrlRepository.java
@@ -2,6 +2,7 @@ package net.codejava;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,13 +11,22 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface TInyUrlRepository extends CrudRepository<TinyUrl,Long> {
-     org.springframework.data.domain.Pageable sortByname=  PageRequest.of(0,3, Sort.by("mainurl").ascending());
+public interface TInyUrlRepository extends JpaRepository<TinyUrl,Long> {
+    // org.springframework.data.domain.Pageable sortByname=  PageRequest.of(0,3, Sort.by("mainurl").ascending());
 
      @Query(value = "SELECT * FROM TINYURL  where mainurl = ?1",nativeQuery = true)
     public  List<TinyUrl> findByMainUrl(@Param("mainUrl") String mainUrl);
 
+      @Query(value = "SELECT  *  from TINYURL t where t.tinyurl = ?1",nativeQuery = true)
+        public List<TinyUrl> findMainUrlByTinyUrl(@Param("tinyurl") String tinyurl);
 
+
+    @Query(value = "SELECT  *  from TINYURL t where t.tinyurl = ?1",nativeQuery = true)
+    public  TinyUrl getByTinyUrl(@Param("tinyurl") String tinyurl);
+
+     public List<TinyUrl> findByTinyUrl(String tinyurl);
 
 }
